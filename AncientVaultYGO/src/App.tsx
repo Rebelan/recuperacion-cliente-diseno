f83ec8e46b38d0bf129841fd02adc2f56edf9ca0
Rebelan <abelcm2013@gmail.com>
2026-04-23 13:53:51 +0200

    feat: Componente card shadcn ui añadido

--- AncientVaultYGO/src/App.tsx
+++ AncientVaultYGO/src/App.tsx
@@ -1,11 +1,19 @@
 import './App.css'
+import { Button } from './components/ui/button'
+import { Card, CardContent, CardDescription, CardHeader, CardTitle } from './components/ui/card'
 
 function App() {
 
 
   return (
     <>
-      
+      <main className='min-h-screen bg-background text-foreground flex items-center justify-center px-6'>
+        <Card className='w-full max-w-md'>
+          <CardHeader>
+            <CardTitle>AncientVault YGO</CardTitle>
+          </CardHeader>
+        </Card>
+      </main>
     </>
   )
 }
